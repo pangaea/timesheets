094ff6e6319fe02d5bd74ceb5c9200066dcbf172
pangaea <kjac@optonline.net>
2011-01-10 03:45:38 -0500

    fixed rounding issue round 6

--- app/models/timesheet.rb
+++ app/models/timesheet.rb
@@ -1,6 +1,6 @@
 class Timesheet < ActiveRecord::Base
     belongs_to :user
-    has_many :timesheet_days
+    has_many :timesheet_days, :order => "date_worked"
 
     accepts_nested_attributes_for :timesheet_days, :allow_destroy => true,
         :reject_if => proc { |attributes| attributes['date_worked'].blank? }
