76a236c3045d2fb473a28f707b58021f00cbf782
pangaea <kjac@optonline.net>
2011-01-10 01:44:24 -0500

    bug fixes

--- app/controllers/timesheets_controller.rb
+++ app/controllers/timesheets_controller.rb
@@ -20,8 +20,8 @@ class TimesheetsController < ApplicationController
     def show
 #        @timesheet = Timesheet.find(params[:id])
         respond_to do |format|
-            format.html # show.html.erb
-            format.xml  { render :xml => @timesheet }
+            #format.html # show.html.erb
+            #format.xml  { render :xml => @timesheet }
             format.js{}
         end
     end
